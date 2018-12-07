e41328f63ad1bb612b7d4e09c3fc9484fdb684ea
Yugo Matsuo <u5m21j@gmail.com>
2018-12-07 13:03:43 +0900

    to_intsメソッドをmapメソッドでリファクタリング

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,5 +1,5 @@
-def to_hex(r,g,b)
-  [r,g,b].inject('#') do |hex, n|
+def to_hex(r, g, b)
+  [r, g, b].inject('#') do |hex, n|
     hex + n.to_s(16).rjust(2, '0')
   end
 end
@@ -8,9 +8,7 @@ def to_ints(hex)
   r = hex[1..2]
   g = hex[3..4]
   b = hex[5..6]
-  ints = []
-  [r, g, b].each do |s|
-    ints << s.hex
+  [r, g, b].map do |s|
+    s.hex
   end
-  ints
 end
